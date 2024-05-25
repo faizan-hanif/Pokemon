1f2a6b8e82f38c503edc8401c4494ee2a2105735
Faizan Hanif <faizanhanif43@hotmail.com>
2024-05-26 01:56:38 +0400

    contact details updated.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.png';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 import React from "react";
 import Navbar from "./components/NavBar";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/about";
 import Contact from "./pages/contact";
@@ -15,28 +11,28 @@ function App() {
     <div className="App">
       <div class="app-main">
         <Router>
-        <div>
-          <div class="App-header">
-        <div class="logo-div">
-        <img src={logo} className="App-logo" alt="logo" />
-        </div>
-          <div class="nav-div">
-            <Navbar />
-            </div>
+          <div>
+            <div class="App-header">
+              <div class="logo-div">
+                <img src={logo} className="App-logo" alt="logo" />
+              </div>
+              <div class="nav-div">
+                <Navbar />
+              </div>
             </div>
             <div class="app-main">
-            <Routes>
+              <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
-            </Routes>
+              </Routes>
             </div>
-            </div>
+          </div>
         </Router>
       </div>
       <footer class="footer">
-          <p class="footer-text">2023 © All right reserved.</p>
-        </footer>
+        <p class="footer-text">2024 © All right reserved.</p>
+      </footer>
     </div>
   );
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,17 @@
-
 import React from "react";
 
 const Contact = () => {
-	return (
-        <section class="main contact">
-        <h1 class="main-header">Contact Us</h1>
-        <p><a href="mailto:faizanhanif43@hotmail.com">faizanhanif43@hotmail.com</a></p>
-        <p><a href="tel:+923346587788">+92 334 658 7788</a></p>
-      </section>
-	);
+  return (
+    <section class="main contact">
+      <h1 class="main-header">Contact Us</h1>
+      <p>
+        <a href="mailto:faizanhanif43@hotmail.com">faizanhanif43@hotmail.com</a>
+      </p>
+      <p>
+        <a href="tel:+971528605835">+971 52 860 5835</a>
+      </p>
+    </section>
+  );
 };
 
 export default Contact;
